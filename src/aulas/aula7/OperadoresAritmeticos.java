49da4625a6959cc98f18ec250b97acef832963b2
LucasR022 <lucas.roberto200@gmail.com>
2023-06-22 19:41:45 -0300

    aula 07

---
+++ src/aulas/aula7/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+package aulas.aula7;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+
+        System.out.println(m);
+
+        // Operadores Unários
+
+
+        int numero= 5;
+        int valor = 5 + ++numero;
+
+        System.out.println(valor);
+
+        // operadores de atribuição
+
+        int x = 4;
+        x += 2; // igual a x = x + 2;
+
+        System.out.println(x);
+
+
+    }
+}
